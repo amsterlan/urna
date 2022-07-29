d12f8bf75aaa09d4d93d646a34499b2a704ef53f
Alan Barbiero <101684401+amsterlan@users.noreply.github.com>
2022-07-29 02:11:07 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VICE-PRESIDENTE',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                name: 'Fulano de tal',
+                partido: 'ABC',
+                fotos: [
+                    {url:'bono.jpg', legenda:'Vice-presidente',small:false}
+                ]        
+            },
+            {
+                numero: '35484',
+                name: 'beltrano da silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'deputado.jpg', legenda:"Vice-presidente"}
+                ]
+            },
+        ]
+    },
+    {
+        titulo:'PRESIDENTE',
+        numeros:2,
+        candidatos: [
+            {
+                numero:'99',
+                name:'Almirante',
+                partido:'ABC',
+                vice: 'Fulano de tal',
+                fotos:[
+                    {url:'pres.jpg', legenda:'Presidente',small:false},
+                    {url:'bono.jpg', legenda:'Vice-presidente',small:true}
+                ]
+            },
+            {
+                numero:'84',
+                name:'Gerson',
+                partido:'QWERTY',
+                vice:'Mendonça',
+                fotos:[
+                    {url:'gerson.jpg',legenda:'Presidente'},
+                    {url:'bono.jpg',legenda:'Vice-Presidente',small:true}
+                ]
+            }
+        ]
+    }
+]
+
